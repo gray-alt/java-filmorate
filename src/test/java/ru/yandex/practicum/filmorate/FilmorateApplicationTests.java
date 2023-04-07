ce3b41d13112ebaeb442b1f974929bd1ba9c1d9e
gray-alt <gray-alt@yandex.ru>
2023-04-07 10:56:38 +0300

    feat: Добавить контроллеры фильмов и пользователей

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FilmorateApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
