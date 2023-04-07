856c3736bb6720cc8d5da9104350d78914382839
gray-alt <gray-alt@yandex.ru>
2023-04-07 10:56:02 +0300

    feat: Добавить контроллеры фильмов и пользователей

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/DateEqualsOrAfterValidator.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateEqualsOrAfterValidator implements ConstraintValidator<DateEqualsOrAfter, LocalDate> {
+    private String value;
+
+    @Override
+    public void initialize(DateEqualsOrAfter constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+        value = constraintAnnotation.value();
+    }
+
+    @Override
+    public boolean isValid(LocalDate s, ConstraintValidatorContext constraintValidatorContext) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(value, dateTimeFormatter);
+
+        if (s != null && (s.isEqual(localDate) || s.isAfter(localDate))) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/ValidatingService.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.model;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
+@Service
+public class ValidatingService {
+
+    private Validator validator;
+
+    public ValidatingService(Validator validator) {
+        this.validator = validator;
+    }
+
+    public void validateSimpleUser(User.SimpleUser user) throws ValidationException {
+        Set<ConstraintViolation<User.SimpleUser>> violations = validator.validate(user);
+        if (!violations.isEmpty()) {
+            throw new ValidationException(violations.toArray().toString());
+        }
+    }
+
+    public void validateSimpleFilm(Film.SimpleFilm film) throws ValidationException {
+        Set<ConstraintViolation<Film.SimpleFilm>> violations = validator.validate(film);
+        if (!violations.isEmpty()) {
+            throw new ValidationException(violations.toArray().toString());
+        }
+    }
+}
