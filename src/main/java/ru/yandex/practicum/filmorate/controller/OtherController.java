ac01a6010e84f66885fc4dc26022f4f0bec71a34
gray-alt <gray-alt@yandex.ru>
2023-05-25 20:02:07 +0300

    fix: исправить структуру проекта

--- src/main/java/ru/yandex/practicum/filmorate/controller/OtherController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/OtherController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.film.service.FilmService;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
 import java.util.Optional;
