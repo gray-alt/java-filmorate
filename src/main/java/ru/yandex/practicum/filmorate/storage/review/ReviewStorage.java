e80a5459df01f7e2d3d37db3d3f544a5eb9c82f7
gray-alt <gray-alt@yandex.ru>
2023-06-08 11:52:03 +0300

    fix: удалить не используемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.review;
 
-import ru.yandex.practicum.filmorate.controller.ReviewController;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import java.util.Collection;
